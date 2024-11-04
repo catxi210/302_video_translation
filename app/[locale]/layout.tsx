573f7c4f9edf206944011d82f0e3b35ea1179987
zhangpeilin <zhangpeilin@adswave.com>
2024-11-04 11:23:21 +0800

    fix: when NEXT_PUBLIC_SHOW_BRAND is 'false' then remove chat icon

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,10 +1,10 @@
-import { NextIntlClientProvider } from "next-intl";
-import { getMessages } from "next-intl/server";
-import "../globals.css";
 import { ErrorHandler } from "@/components/error-handler";
 import { type LogLevel, LoggerProvider } from "@/util/logger";
+import { NextIntlClientProvider } from "next-intl";
+import { getMessages } from "next-intl/server";
 import type { ReactNode } from "react";
 import { Toaster } from "react-hot-toast";
+import "../globals.css";
 
 export default async function RootLayout({
   children,
@@ -14,7 +14,7 @@ export default async function RootLayout({
   params: { locale: string };
 }>) {
   const messages = await getMessages();
-
+  const showBrand = process.env.NEXT_PUBLIC_SHOW_BRAND === "true";
   return (
     <html lang={locale}>
       <body className="min-h-screen bg-[#f5f5f5] min-w-full h-full">
@@ -25,7 +25,7 @@ export default async function RootLayout({
             {children}
           </LoggerProvider>
         </NextIntlClientProvider>
-        <script src="https://assets.salesmartly.com/js/project_177_61_1649762323.js" />
+        {showBrand && <script src="https://assets.salesmartly.com/js/project_177_61_1649762323.js" />}
       </body>
     </html>
   );
